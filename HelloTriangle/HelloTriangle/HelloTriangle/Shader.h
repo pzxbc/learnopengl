b0a8a67e5285932dc570347c0bde5e9e055e5fa3
pzxbc <pzxbc@qq.com>
2019-05-23 23:18:21 +0800

    upgrade vs2019

---
+++ HelloTriangle/HelloTriangle/HelloTriangle/Shader.h
@@ -0,0 +1,26 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath)
+	{
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	}
+};
+
+#endif
